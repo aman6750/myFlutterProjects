855632186604eae0aa31850ca67e0aadc261d497
Aman <amanyadav6750@gmail.com>
2023-11-04 17:55:02 +0530

    making an instagram clone

---
+++ instagram_clone/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Text('Lets build an Instagram'),
+      ),
+    );
+  }
+}
