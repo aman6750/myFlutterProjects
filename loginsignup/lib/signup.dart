464e4a93bb6fbe6ea6b7eaf423dd3babf2e72f7f
Aman <amanyadav6750@gmail.com>
2023-12-06 19:46:19 +0530

    starting registration page

---
+++ loginsignup/lib/signup.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SingUp Api'),
+      ),
+    );
+  }
+}
